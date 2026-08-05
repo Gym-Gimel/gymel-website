67b68d6e95d1d61464ab492227d72e74cc1f907e
ddieperi <david.dieperink@ik.me>
2026-08-05 13:49:20 +0200

    style: made the main content flex to stick the footer to the bottom of the site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="fr">
-      <body className="min-h-screen antialiased">
+      <body className="flex min-h-screen flex-col antialiased">
         <Header />
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
